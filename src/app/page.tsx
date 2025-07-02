3be8df4ec52cf67b7c04c6cdac17df225c841b40
lucadude99 <lvir@dude.it>
2025-07-02 18:03:57 +0200

    posizionamenti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,59 +17,63 @@ export default function Home() {
         <Image src="/rude_logo.png" alt="RUDE logo" width={200} height={100} />
       </motion.div>
 
-             {/* Frasi + Lapide */}
-       <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-5xl mb-8 relative">
-         {/* Frase sinistra - sopra su mobile, a sinistra su desktop */}
-         <motion.p
-           initial={{ x: '-100vw', opacity: 0 }}
-           animate={{ x: 0, opacity: 1 }}
-           transition={{ delay: 1, duration: 2 }}
-           className="text-xl md:text-xl mb-4 md:mb-0 md:mr-4 whitespace-nowrap"
-         >
-           forse quelli della mala
-         </motion.p>
+      {/* Frasi + Lapide */}
+      {/* Frasi + Lapide */}
+      <div className="w-full max-w-5xl mb-6 flex flex-col md:grid md:grid-cols-3 items-center justify-center text-center">
+        {/* Frase sinistra */}
+        <motion.p
+          initial={{ x: '-100vw', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 1, duration: 1 }}
+          className="text-base md:text-xl mb-2 md:mb-0 md:justify-self-end whitespace-nowrap"
+        >
+          forse quelli della mala
+        </motion.p>
 
-         {/* Lapide - già visibile senza animazioni */}
-         <div>
-           <Image src="/lapide.png" alt="Lapide" width={300} height={300} />
-         </div>
+        {/* Lapide */}
+        <div className="flex justify-center">
+          <Image src="/lapide.png" alt="Lapide" width={200} height={200} className="md:w-[300px] md:h-[300px]" />
+        </div>
 
-         {/* Frase destra - sotto su mobile, a destra su desktop */}
-         <motion.p
-           initial={{ x: '100vw', opacity: 0 }}
-           animate={{ x: 0, opacity: 1 }}
-           transition={{ delay: 1, duration: 2 }}
-           className="text-xl md:text-xl mt-4 md:mt-0 md:ml-4 whitespace-nowrap"
-         >
-           forse la pubblicità
-         </motion.p>
-       </div>
+        {/* Frase destra */}
+        <motion.p
+          initial={{ x: '100vw', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 1, duration: 1 }}
+          className="text-base md:text-xl mt-2 md:mt-0 md:justify-self-start whitespace-nowrap"
+        >
+          forse la pubblicità
+        </motion.p>
+      </div>
 
       {/* Icone Social */}
       <motion.div
-        className="flex gap-10 mt-4"
+        className="flex gap-6 md:gap-10 mt-2 mb-2 md:mb-4"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 0.5, duration: 2 }}
+        transition={{ delay: 0.5, duration: 1 }}
       >
         <Link href="https://www.instagram.com/rude___team/" target="_blank" rel="noopener noreferrer">
-          <Image src="/instagram.svg" alt="Instagram" width={32} height={32} />
+          <Image src="/instagram.svg" alt="Instagram" width={24} height={24} className="md:w-8 md:h-8" />
         </Link>
         <Link href="https://www.linkedin.com/company/rudeteam/" target="_blank" rel="noopener noreferrer">
-          <Image src="/linkedin.svg" alt="LinkedIn" width={32} height={32} />
+          <Image src="/linkedin.svg" alt="LinkedIn" width={24} height={24} className="md:w-8 md:h-8" />
         </Link>
         <a href="mailto:info@rude.team">
-          <Image src="/mail.svg" alt="Email" width={32} height={32} />
+          <Image src="/mail.svg" alt="Email" width={24} height={24} className="md:w-8 md:h-8" />
         </a>
       </motion.div>
-             <motion.p
-           className="text-lg md:text-xl mt-4"
-           initial={{ opacity: 0 }}
-           animate={{ opacity: 1 }}
-           transition={{ delay: 0.5, duration: 2 }}
-         >
-           via arquà 13, milano
-         </motion.p>
+
+      {/* Indirizzo */}
+      <motion.p
+        className="text-sm md:text-lg mt-1"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+      >
+        via arquà 13, milano
+      </motion.p>
+
     </main>
   )
 }
