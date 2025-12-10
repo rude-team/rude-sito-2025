2f6fbc2282b91de7b3ab0546726f1ce3770ecbf0
lucadude99 <lvir@dude.it>
2025-12-10 18:15:32 +0100

    Update image paths and Next.js image config
    Renamed local thumbnail images to new locations under 'public/img-lavori' and updated their references in projects.json to use Google Cloud Storage URLs. Added remotePatterns for Google Cloud Storage assets in next.config.ts to enable remote image loading.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/asset_sito_rude/**', // Puoi essere più specifico, ma ** è un jolly che copre tutti i percorsi sotto il tuo bucket
+      },
+    ],
+  },
 };
 
 export default nextConfig;
