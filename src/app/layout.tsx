444539181fb8b96df561565e60575286a6abacb5
lucadude99 <lvir@dude.it>
2026-01-27 14:33:41 +0100

    Add 'Mad Sandro' project and fix metadata description
    Added a new project entry 'Mad Sandro' for client Sibill in projects.json, including details, media, and tags. Also fixed an escape character in the metadata description in layout.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
     default: 'rude | bottega creativa',
     template: '%s | rude',
   },
-  description: 'forse quelli della mala, forse la pubblicità ¯\\_(ツ)_/¯',
+  description: 'forse quelli della mala, forse la pubblicità ¯\_(ツ)_/¯',
   keywords: ['pubblicità', 'creatività', 'advertising', 'marketing', 'brand', 'comunicazione'],
   authors: [{ name: 'rude' }],
   creator: 'rude',
